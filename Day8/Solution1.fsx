66b7884507095a0b1088ac41e50f7c636697d9b4
Christian Sunesen <christian_sunesen@hotmail.com>
2023-12-09 16:04:51 +0100

    Day 8 Solution 1

--- Day8/Solution1.fsx
+++ Day8/Solution1.fsx
@@ -1,18 +1,40 @@
 open System.IO
 
-let lines = File.ReadAllLines("Day8/testInput.txt")
-
-let moves = lines[0] |> Seq.toArray
-
-type Node = {
-    Name: string
-    Left: string
-    Right: string
-}
-
+let lines = File.ReadAllLines("Day8/input.txt")
 let rawNodesList = lines[2..]
+let moves = lines[0] |> Seq.toList
 
-let nodesNameList = rawNodesList |> Array.map (fun x -> ((x.Split("="))[0]).Trim())
+let getNextNode (nodeName:string) =
+    let findNodeAsString = rawNodesList |> Array.find (fun y -> 
+        let name  = (y.Split("=")[0]).Trim()
+        name = nodeName
+        )
+    let leftAndRight = findNodeAsString.Split("(")[1]
+    let left = leftAndRight.Split(",")[0]
+    let right = ((leftAndRight.Split(",")[1]).Trim().Substring(0, 3))
+    (nodeName, left, right)
+
+let findNumSteps (moves:char list) (node:string) =
+    let rec test (nextMoves:char list) numSteps (currentNode:string) =
+        match nextMoves with
+        | head::tail -> 
+            match head with
+            | 'L' -> 
+                let (head, left, right) = getNextNode currentNode
+                if left = "ZZZ" then numSteps else test tail (numSteps + 1) left
+            | 'R' -> 
+                let (head, left, right) = getNextNode currentNode
+                if right = "ZZZ" then numSteps else test tail (numSteps + 1) right
+            | _ -> 
+                test tail numSteps currentNode
+        | [] when numSteps = 10 -> numSteps
+        | [] -> 
+            test moves numSteps currentNode
+    test moves 1 node
+
+printfn "%A" moves
+printfn "%A" rawNodesList
+findNumSteps moves "AAA"
 
 
     
